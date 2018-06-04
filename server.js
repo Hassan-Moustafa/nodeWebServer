843883c12221606aa239a1e0e0a95ab717e3fdc8
hassan <hassanmoustafa40@yahoo.com>
2018-06-04 07:42:25 -0700

    remove update middleware

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use((req , res , next) => {
     next();
 })
 
-app.use((req,res) => {
-    res.render('updating.hbs');
-})
+// app.use((req,res) => {
+//     res.render('updating.hbs');
+// })
 
 app.get('/',(req , res) =>{
     res.send({
